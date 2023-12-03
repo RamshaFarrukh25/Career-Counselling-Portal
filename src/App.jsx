09b073eb114cce7a2654d897d7c6e01a91ba5a7d
Mehak Nadeem <mehaknadeem483@gmail.com>
2023-12-03 16:55:49 +0500

    BlogCards file added

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,15 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import "./assets/styles/Global.module.css"
 
 // Import Pages
-import Hero from './pages/Hero'
+//import Hero from './pages/Hero'
+import BlogCards from './pages/BlogCards'
 
 export default function App() {
   return (
     <>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Hero />}/>
+          <Route path="/" element={<BlogCards/>}/>
         </Routes>
       </BrowserRouter>
     </>
--- src/pages/BlogCards.jsx
+++ src/pages/BlogCards.jsx
@@ -1,7 +1,68 @@
 import BlogCardsCSS from "../assets/styles/BlogCards.module.css"
+import backgroundImage from "../assets/images/Blogs_BlogPic.jpg"
 
 export default function BlogCards(){
     return (
-        <h1>Popular Blog Cards by Mehak</h1>
-    )
+        <div className={BlogCardsCSS.overAll}>
+            <div className={BlogCardsCSS.mainHeading}>
+                <h1>Popular Blogs</h1>
+            </div>
+            <div className={BlogCardsCSS.searchBarContainer}>
+                <input type="search" placeholder="Search" className={BlogCardsCSS.searchBarInput}/>
+                <button className={BlogCardsCSS.searchBarBtn}>Search</button>
+            </div>
+            <section className={BlogCardsCSS.cardsWrapper}>
+                <div className={BlogCardsCSS.cardGridSpace}>
+                    <a 
+                        className={BlogCardsCSS.card} 
+                        href="#" 
+                        style={{ backgroundImage: `url(${backgroundImage})` }}
+                    >
+                        <div>
+                            <h1>Blog Title</h1>
+                            <p>The syntax of a language is how it works. How to actually write it. Learn HTML syntax…</p>
+                            <div className={BlogCardsCSS.date}>Blog date</div>
+                            <div className={BlogCardsCSS.tags}>
+                                <a className={BlogCardsCSS.tag} href='#'>Read More...</a>
+                            </div>
+                        </div>
+                    </a>
+                </div>
+
+                <div className={BlogCardsCSS.cardGridSpace}>
+                    <a 
+                        className={BlogCardsCSS.card} 
+                        href="#" 
+                        style={{ backgroundImage: `url(${backgroundImage})` }}
+                    >
+                        <div>
+                            <h1>Blog Title</h1>
+                            <p>The syntax of a language is how it works. How to actually write it. Learn HTML syntax…</p>
+                            <div className={BlogCardsCSS.date}>Blog date</div>
+                            <div className={BlogCardsCSS.tags}>
+                                <a className={BlogCardsCSS.tag} href='#'>Read More...</a>
+                            </div>
+                        </div>
+                    </a>
+                </div>
+
+                <div className={BlogCardsCSS.cardGridSpace}>
+                    <a 
+                        className={BlogCardsCSS.card} 
+                        href="#" 
+                        style={{ backgroundImage: `url(${backgroundImage})` }}
+                    >
+                        <div>
+                            <h1>Blog Title</h1>
+                            <p>The syntax of a language is how it works. How to actually write it. Learn HTML syntax…</p>
+                            <div className={BlogCardsCSS.date}>Blog date</div>
+                            <div className={BlogCardsCSS.tags}>
+                                <a className={BlogCardsCSS.tag} href='#'>Read More...</a>
+                            </div>
+                        </div>
+                    </a>
+                </div>
+            </section>
+        </div>
+  );
 }
