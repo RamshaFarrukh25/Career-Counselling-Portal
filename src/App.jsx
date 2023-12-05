e3af2d0a38a3d80ba532bffa803a26a17a9e6ee9
RamshaFarrukh25 <ramshafarrukh1051@gmail.com>
2023-12-05 18:47:20 +0500

    Header and Hero Section added

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,15 @@ import "./assets/styles/Global.module.css"
 
 // Import Pages
 import Hero from './pages/Hero'
+import Header from './layouts/Header'
 
 export default function App() {
   return (
     <>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Hero />}/>
+          <Route path="/" element={<Header/>} /> 
+          <Route path="/hero" element={<Hero/>}/>
         </Routes>
       </BrowserRouter>
     </>
