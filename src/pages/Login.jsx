fb0aac15493cf8ac9a0765b91ff2619ed2dfd14a
Laiba <laibatariq729@gmail.com>
2023-12-06 18:50:51 +0500

    Signup and Login page

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,7 +1,77 @@
+import { useDispatch, useSelector } from "react-redux"
+import { 
+  handleChange,
+  handleSubmit 
+} from "../features/login/loginSlice"
+import { Link } from "react-router-dom"
 import LoginCSS from "../assets/styles/Login.module.css"
+import Image1 from "../assets/images/Login_Image1.gif"
 
-export default function Login(){
+
+export default function Login() {
+    const dispatch = useDispatch()
+    const {loginForm} = useSelector((store) => store.login)
     return (
-        <h1>Login By Laiba</h1>
+      <div className={LoginCSS.wrapper}>
+        <div className={LoginCSS.inner}>
+          <img src={Image1} alt="" className={LoginCSS.image1} />
+
+          <form 
+            className={LoginCSS.form}
+            onSubmit={(event) => {
+                event.preventDefault()
+                dispatch(handleSubmit())
+            }}
+          >
+            <h2 className={LoginCSS.loginHeading}>Login</h2>
+            <div className={LoginCSS.formHolder}>
+              <span>
+                <i className="fa-regular fa-envelope"></i>
+              </span>
+              <input
+                type="email"
+                className={LoginCSS.formControl}
+                value={loginForm.email}
+                placeholder="Email"
+                name="email"
+                onChange={(event) => {
+                    dispatch(handleChange({
+                        name: event.target.name,
+                        value: event.target.value
+                    }))
+                }}
+                required
+              />
+            </div>
+            <div className={LoginCSS.formHolder}>
+              <span>
+                <i className="fa-solid fa-lock"></i>
+              </span>
+              <input
+                type="password"
+                className={LoginCSS.formControl}
+                value={loginForm.password}
+                placeholder="Password"
+                name="password"
+                onChange={(event) => {
+                    dispatch(handleChange({
+                        name: event.target.name,
+                        value: event.target.value
+                    }))
+                }}
+                required
+              />
+            </div>
+            <button className={LoginCSS.loginBtn}>
+              <span>Login</span>
+            </button>
+
+            <p className={LoginCSS.accountExist}>
+              Not a member?
+              <Link to="/signup">Signup</Link>
+            </p>
+          </form>
+        </div>
+      </div>
     )
 }
--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,7 +1,140 @@
+import { useDispatch, useSelector } from "react-redux"
+import { 
+  handleChange, 
+  handleSubmit, 
+  clearForm,
+  matchPasswords
+} from "../features/signup/signupSlice"
+import { Link } from "react-router-dom"
 import SignupCSS from "../assets/styles/Signup.module.css"
+import Image1 from "../assets/images/Signup_Image1.png"
+import Robo from "../assets/images/Signup_Robo.png"
 
-export default function Signup(){
-    return (
-        <h1>Signup by Laiba</h1>
-    )
+
+export default function Signup() {
+  const dispatch = useDispatch()
+  const { signupForm, passwordMatch } = useSelector((store) => store.signup)
+  return (
+    <div className={SignupCSS.wrapper}>
+      <div className={SignupCSS.inner}>
+        <img src={Image1} alt="" className={SignupCSS.image1} />
+
+        <form 
+          onSubmit={(event) => { 
+            event.preventDefault()
+            dispatch(handleSubmit())
+            dispatch(clearForm())
+          }} 
+          className={SignupCSS.form}
+        >
+          <h2 className={SignupCSS.signupHeading}>Sign Up</h2>
+          <div className={SignupCSS.formHolder}>
+            <span>
+              <i className="fa-regular fa-user"></i>
+            </span>
+            <input
+              type="text"
+              className={SignupCSS.formControl}
+              placeholder="Name"
+              value={signupForm.name}
+              name="name"
+              onChange={(event) =>
+                dispatch(
+                  handleChange({
+                    name: event.target.name,
+                    value: event.target.value,
+                  })
+                )
+              }
+              required
+            />
+          </div>
+          <div className={SignupCSS.formHolder}>
+            <span>
+              <i className="fa-regular fa-envelope"></i>
+            </span>
+            <input
+              type="email"
+              className={SignupCSS.formControl}
+              placeholder="Email"
+              value={signupForm.email}
+              name="email"
+              onChange={(event) =>
+                dispatch(
+                  handleChange({
+                    name: event.target.name,
+                    value: event.target.value,
+                  })
+                )
+              }
+              required
+            />
+          </div>
+          <div className={SignupCSS.formHolder}>
+            <span>
+              <i className="fa-solid fa-lock"></i>
+            </span>
+            <input
+              type="password"
+              className={`
+                ${SignupCSS.formControl} 
+                ${SignupCSS[passwordMatch]}`
+              }
+              placeholder="Password"
+              value={signupForm.password}
+              name="password"
+              onChange={(event) =>
+                dispatch(
+                  handleChange({
+                    name: event.target.name,
+                    value: event.target.value,
+                  })
+                )
+              }
+              minLength="8"
+              pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}"
+              title="Password must contain at least one number and one uppercase and lowercase letter, and at least 8 or more characters"
+              required
+            />
+          </div>
+          <div className={SignupCSS.formHolder}>
+            <span>
+              <i className="fa-solid fa-lock"></i>
+            </span>
+            <input
+              type="password"
+              className={`
+                ${SignupCSS.formControl} 
+                ${SignupCSS[passwordMatch]}`
+              }
+              placeholder="Confirm Password"
+              value={signupForm.confirmPassword}
+              name="confirmPassword"
+              onChange={(event) =>
+                dispatch(
+                  handleChange({
+                    name: event.target.name,
+                    value: event.target.value,
+                  })
+                )
+              }
+              onMouseOut={(event) => dispatch(matchPasswords())}
+              required
+            />
+          </div>
+          <button 
+            className={SignupCSS.signupBtn}
+          >
+            <span>Signup</span>
+          </button>
+
+          <p className={SignupCSS.accountExist}>
+            Already have an account?  
+            <Link to="/login">Login</Link>
+          </p>
+        </form>
+        <img src={Robo} alt="" className={SignupCSS.image2} />
+      </div>
+    </div>
+  )
 }
