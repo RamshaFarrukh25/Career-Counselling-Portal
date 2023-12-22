c0dff7f1d3d7d14b29d2c185cf4b8247e4ec1eb7
Mehak Nadeem <mehaknadeem483@gmail.com>
2023-12-22 10:28:34 +0500

    Reviews And Rating Form
    Reviews about Website and Rating about   Counsellor

--- src/App.jsx
+++ src/App.jsx
@@ -14,11 +14,11 @@ export default function App() {
         <Routes>
           <Route path="/" element={<Layout/>} > 
             <Route index element={<Home />}/>
+            <Route path="blogDetail" element={<BlogDetail />} />
+
            </Route>
           <Route path="login" element={<Login />} />
           <Route path="signup" element={<Signup />} />
-          <Route path="blogDetail" element={<BlogDetail />} />
-
         </Routes>
       </BrowserRouter>
     </>
--- src/store.jsx
+++ src/store.jsx
@@ -1,10 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit"
 import signupReducer from "./features/signup/signupSlice"
 import loginReducer from "./features/login/loginSlice"
+import reviewsReducer from "./features/reviews/reviewsSlice"
 
 export const store = configureStore({
     reducer: {
         signup: signupReducer,
-        login: loginReducer
+        login: loginReducer,
+        reviews: reviewsReducer
     }
 })
