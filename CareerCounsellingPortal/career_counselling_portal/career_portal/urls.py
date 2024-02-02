222bf19783746558f4d21977654f96796c77b5ef
Mehak Nadeem <mehaknadeem483@gmail.com>
2024-02-02 15:46:23 +0500

    Update

--- CareerCounsellingPortal/career_counselling_portal/career_portal/urls.py
+++ CareerCounsellingPortal/career_counselling_portal/career_portal/urls.py
@@ -7,5 +7,4 @@ urlpatterns = [
     path('checkEmail',views.checkEmail,name='checkEmail'),
     path('fetchBlogsData', views.fetchBlogsData, name='fetchBlogsData'),
     path('blogDetails',views.blogDetails,name='blogDetails'),
-    path('saveReviews', views.saveReviews, name = 'saveReviews'),
 ]
